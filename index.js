fb653c34e3803cad514b399c306a77532f76062e
smriti-67-samanta <samantasmriti0@gmail.com>
2025-03-08 12:15:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-//let intervalId=setInterval(()=>{
-//  console.log("Loading..." )
-//},1000)
-//setTimeout(()=>{
-//  clearInterval(intervalId);
-//  console.log("Loaded successfully!")
-//},5000)
+
 
 let intervalId = setInterval(() => {
     console.log("Loading...");
 }, 1000);
 
 setTimeout(() => {
-    clearInterval(intervalId); // Stop the interval
-    console.log("Loaded successfully!"); // Log the success message
+    clearInterval(intervalId); 
+    console.log("Loaded successfully!"); 
 }, 5000);
